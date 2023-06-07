e4cd481be18021387d1ae6a425d4ad7299982478
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-06-07 21:22:07 +0100

    user page title styled

--- src/pages/user/[uid].js
+++ src/pages/user/[uid].js
@@ -1,6 +1,16 @@
 import { useRouter } from 'next/router';
+import Typography from '@mui/material/Typography';
 
 export default function Page() {
   const router = useRouter();
-  return <p>User: {router.query.uid}</p>;
+  return (
+    <>
+      <Typography component="h2" variant="h4" className='text-center mt-4 mb-8 underline'>
+        My Surveys
+      </Typography>
+      <div className='flex justify-center'>
+        You have no surveys.
+      </div>
+    </>
+  );
 }
