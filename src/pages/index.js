d3a18795128712b96cd246e651f424e0c3b7a1b9
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-06-08 11:43:54 +0100

    create survey link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
+import { useRouter } from 'next/router';
+import useStore, { selectUser } from '@/store/store';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 export default function Home() {
+  let router = useRouter();
+  const user = useStore(selectUser);
+
+  const handleCreateSurveyClick = () => {
+    if (user) {
+      router.push('/create')
+    } else {
+      router.push('/sign-up')
+    }
+  }
+
   return (
     <div className="h-full w-full flex justify-center items-center">
       <div className="flex flex-col items-center gap-8">
@@ -9,10 +22,21 @@ export default function Home() {
           Welcome to Surveyor. What would you like to do?
         </Typography>
         <div className="flex gap-24">
-          <Button variant="contained" color="primary" size="large" sx={{borderRadius: '12px'}}>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{ borderRadius: '12px' }}
+            onClick={() => handleCreateSurveyClick()}
+          >
             Create a Survey
           </Button>
-          <Button variant="contained" color="secondary" size="large" sx={{borderRadius: '12px'}}>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            sx={{ borderRadius: '12px' }}
+          >
             Answer a Survey
           </Button>
         </div>
