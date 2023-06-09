9a7fa28040a783762803aba8034b45f874704c4b
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-06-09 23:37:21 +0100

    changed process variable

--- src/lib/api-functions/server/api.js
+++ src/lib/api-functions/server/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const {
-  API_URL='http://localhost:3000'
+  API_KEY='http://localhost:3000'
 } = process.env;
 
-export const SURVEYS_ENDPOINT = `${API_URL}/api/v1/surveys/`;
+export const SURVEYS_ENDPOINT = `${API_KEY}/api/v1/surveys/`;
 
 export const addSurvey = async (data) => {
   const response = await axios({
