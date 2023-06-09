3cac7337f21d77959a10d2fc0e4973039c7d86a0
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-06-09 23:05:27 +0100

    added cross env

--- src/lib/api-functions/server/api.js
+++ src/lib/api-functions/server/api.js
@@ -1,16 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
-const {
-  host='http://localhost:3000'
-} = process.env;
+const { VERCEL_BRANCH_URL='http://localhost:3000' } = process.env;
 
-export const SURVEYS_ENDPOINT = `${host}/api/v1/surveys/`;
+let SURVEYS_ENDPOINT = `${host}/api/v1/surveys/`;
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'development') {
+  SURVEYS_ENDPOINT = `${VERCEL_BRANCH_URL}/api/v1/surveys/`
+} else {
+  SURVEYS_ENDPOINT = `https://${VERCEL_BRANCH_URL}/api/v1/surveys/`
+}
+
+export { SURVEYS_ENDPOINT };
 
 export const addSurvey = async (data) => {
   const response = await axios({
-    method: "POST",
+    method: 'POST',
     url: SURVEYS_ENDPOINT,
-    data
+    data,
   });
-  return response.data
-}
+  return response.data;
+};
