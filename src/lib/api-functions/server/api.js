08af379fa75c736454ba11ca106e1b55f79477cd
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-06-09 22:45:12 +0100

    process variable change

--- src/lib/api-functions/server/api.js
+++ src/lib/api-functions/server/api.js
@@ -1,16 +1,10 @@
 import axios from "axios";
 
-// const {
-//   host='http://localhost:3000'
-// } = process.env;
-
 const {
-  VERCEL_URL='http://localhost:3000'
+  host='http://localhost:3000'
 } = process.env;
 
-console.log(VERCEL_URL)
-
-export const SURVEYS_ENDPOINT = `${VERCEL_URL}/api/v1/surveys/`;
+export const SURVEYS_ENDPOINT = `${host}/api/v1/surveys/`;
 
 export const addSurvey = async (data) => {
   const response = await axios({
