3892fe82a1280d597296a67069815c54fbf38b16
aaronwp21 <awhitehousepope@gmail.com>
2023-06-17 20:28:05 +0100

    questions listed

--- src/pages/user/answers/[id].js
+++ src/pages/user/answers/[id].js
@@ -1,24 +1,54 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import Box from '@mui/material/Box';
 
 function Page() {
+  const origin = window.location.origin;
+  const id = window.location.href.match(/(?<=answer.)[^\]]+/)[0];
+  const path = `${origin}/api/v1/answers/${id}`;
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['survey'],
+    queryFn: () =>
+      fetch(path).then((res) => {
+        return res.json();
+      }),
+  });
+  
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  const questions = data.surveys[1];
+
   return (
     <nav aria-label="main mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Inbox" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Drafts" />
-            </ListItemButton>
-          </ListItem>
-        </List>
+        <Box sx={{width: '80%', margin: 'auto', marginBlockStart: '2rem'}}>
+          <List>
+            {questions.map((question, i) => {
+              return (
+                <ListItem disablePadding key={i}>
+                  <ListItemButton>
+                    <ListItemText primary={question.question} />
+                  </ListItemButton>
+                </ListItem>
+              )
+            })}
+            {/* <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Drafts" />
+              </ListItemButton>
+            </ListItem> */}
+          </List>
+        </Box>
       </nav>
   )
 }
